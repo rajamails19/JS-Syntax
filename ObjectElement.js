7801f542df97513f6c47841bf90a52e8c45e4ad9
Raja V <rajamails19@gmail.com>
2023-12-31 12:33:22 -0800

    Create ObjectElement.js

---
+++ ObjectElement.js
@@ -0,0 +1,15 @@
+
+const person = { name: 'John', age: 30, job: 'developer' };
+ 
+for (let key in person) 
+{
+console.log(key, person [key]);
+}
+
+/*
+
+"name", "John"
+"age", 30
+"job", "developer"
+
+*/
